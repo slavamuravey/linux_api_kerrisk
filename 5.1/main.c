cbc53fd152787975a8d7e28589160f1b70266964
slavamuravey <slavamuravey@mail.ru>
2023-10-17 17:17:15 +0700

    Finish 5.1 task

--- 5.1/main.c
+++ 5.1/main.c
@@ -1,5 +1,6 @@
 #define _LARGEFILE64_SOURCE
 #define _ISOC99_SOURCE
+#define _FILE_OFFSET_BITS 64
 #include <sys/stat.h>
 #include <fcntl.h>
 #include "../lib/tlpi_hdr.h"
@@ -7,19 +8,19 @@
 int main(int argc, char *argv[])
 {
     int fd;
-    off64_t off;
+    off_t off;
     if (argc != 3 || strcmp(argv[1], "--help") == 0) {
         usageErr("%s pathname offset\n", argv[0]);
     }
     
-    fd = open64(argv[1], O_RDWR | O_CREAT, S_IRUSR | S_IWUSR);
+    fd = open(argv[1], O_RDWR | O_CREAT, S_IRUSR | S_IWUSR);
     if (fd == -1) {
-        errExit("open64");
+        errExit("open");
     }
     
     off = atoll(argv[2]);
-    if (lseek64(fd, off, SEEK_SET) == -1) {
-        errExit("lseek64");
+    if (lseek(fd, off, SEEK_SET) == -1) {
+        errExit("lseek");
     }
     
     if (write(fd, "test", 4) == -1) {
--- include/main.mk
+++ include/main.mk
@@ -1,5 +1,5 @@
 CC = gcc
-CFLAGS = -g -Wall -ansi -pedantic
+CFLAGS ?= -g -Wall -ansi -pedantic
 SRCMODULES ?= 
 OBJMODULES = $(SRCMODULES:.c=.o)
 
