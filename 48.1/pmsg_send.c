e5b0f9c172b59729259b1be0d22c49dcec225d0f
slavamuravey <slavamuravey@mail.ru>
2024-03-10 20:12:08 +0700

    Add listing code

---
+++ 48.1/pmsg_send.c
@@ -0,0 +1,46 @@
+#include <mqueue.h>
+#include <fcntl.h>              /* For definition of O_NONBLOCK */
+#include <unistd.h>
+#include <getopt.h>
+#include "../lib/tlpi_hdr.h"
+
+extern int optind;
+
+static void usageError(const char *progName)
+{
+    fprintf(stderr, "Usage: %s [-n] mq-name msg [prio]\n", progName);
+    fprintf(stderr, "    -n           Use O_NONBLOCK flag\n");
+    exit(EXIT_FAILURE);
+}
+
+int main(int argc, char *argv[])
+{
+    int flags, opt;
+    mqd_t mqd;
+    unsigned int prio;
+
+    flags = O_WRONLY;
+    while ((opt = getopt(argc, argv, "n")) != -1) {
+        switch (opt) {
+        case 'n':   flags |= O_NONBLOCK;        break;
+        default:    usageError(argv[0]);
+        }
+    }
+
+    if (optind + 1 >= argc) {
+        usageError(argv[0]);
+    }
+
+    mqd = mq_open(argv[optind], flags);
+    if (mqd == (mqd_t) -1) {
+        errExit("mq_open");
+    }
+        
+    prio = (argc > optind + 2) ? atoi(argv[optind + 2]) : 0;
+
+    if (mq_send(mqd, argv[optind + 1], strlen(argv[optind + 1]), prio) == -1) {
+        errExit("mq_send");
+    }
+        
+    exit(EXIT_SUCCESS);
+}
