34f8781ef0d56d9941ee80d40d6fe207c131374b
slavamuravey <slavamuravey@mail.ru>
2024-04-17 12:49:13 +0700

    Finish 58.1 task

---
+++ 58.1/main.c
@@ -0,0 +1,36 @@
+#include <unistd.h>
+#include <termios.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+int my_isatty(int fd)
+{
+    struct termios t;
+    
+    return tcgetattr(fd, &t) == -1 ? 0 : 1;
+}
+
+void test_isatty(int fd)
+{
+    if (my_isatty(fd)) {
+        printf("tty\n");
+    } else {
+        perror("isatty");
+    }
+}
+
+int main(int argc, char *argv[])
+{
+    int fd;
+    fd = open("/dev/null", O_RDONLY);
+    
+    test_isatty(STDIN_FILENO);
+    test_isatty(fd);
+    test_isatty(100500);
+
+    close(fd);
+
+    return 0;
+}
