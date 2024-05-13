8c9ba4e18f400fbbcada893e38fb835dcb0dbeef
slavamuravey <slavamuravey@mail.ru>
2024-05-13 12:23:15 +0700

    Finish 58.4 task

--- 58.4/main.c
+++ 58.4/main.c
@@ -10,11 +10,11 @@ int main(int argc, char *argv[])
     struct termios tp;
 
     if (tcgetattr(STDIN_FILENO, &tp) == -1) {
-        exit(EXIT_FAILURE);
         perror("tcgetattr");
+        exit(EXIT_FAILURE);
     }
 
-    printf("ICANON: %s\n", tp.c_lflag && ICANON ? "true" : "false");
+    printf("ICANON: %u\n", (tp.c_lflag & ICANON));
     printf("TIME: %u, MIN: %u\n", tp.c_cc[VTIME], tp.c_cc[VMIN]);
     
     return EXIT_SUCCESS;
